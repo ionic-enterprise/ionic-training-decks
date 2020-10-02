f1c17b70bf29e34c9b5fdaaf145a02599f08d990
Ken Sodemann <ken@ionic.io>
2020-10-02 12:28:13 -0500

    chore(): upgrade the dependencies

--- src/app/about/about.page.spec.ts
+++ src/app/about/about.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { AboutPage } from './about.page';
@@ -7,16 +7,18 @@ describe('AboutPage', () => {
   let component: AboutPage;
   let fixture: ComponentFixture<AboutPage>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [AboutPage],
-      imports: [IonicModule],
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [AboutPage],
+        imports: [IonicModule],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(AboutPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(AboutPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    }),
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
